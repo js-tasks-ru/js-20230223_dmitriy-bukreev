f12c8aa01b6366165d525b373201af6375c37520
DmitriyBukreev <dmitribukree@yandex.ru>
2023-02-28 17:57:36 +0300

    sortStrings function implemented

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -4,6 +4,15 @@
  * @param {string} [param="asc"] param - the sorting type "asc" or "desc"
  * @returns {string[]}
  */
-export function sortStrings(arr, param = 'asc') {
+export function sortStrings(arr, param = "asc") {
+  // Assuming `arr` to be a flat array
+  const result = [...arr];
 
+  if (param === "asc") return result.sort(compareStrings);
+  else if (param === "desc")
+    return result.sort((a, b) => -compareStrings(a, b));
+}
+
+function compareStrings(a, b) {
+  return a.localeCompare(b, ["ru", "en"], { caseFirst: "upper" });
 }
