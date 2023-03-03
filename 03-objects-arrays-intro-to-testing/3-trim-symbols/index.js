8278d22a69e15d75d62058d8a894a33e3fc8c952
DmitriyBukreev <dmitribukree@yandex.ru>
2023-03-03 22:27:31 +0300

    Implemented 'trimSymbols' function

--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -5,5 +5,17 @@
  * @returns {string} - the new string without extra symbols according passed size
  */
 export function trimSymbols(string, size) {
-
+  if (size === undefined) return string;
+  let result = "";
+  let curSize = 0;
+  let prevChar;
+  for (const char of string) {
+    if (char !== prevChar) {
+      prevChar = char;
+      curSize = 0;
+    }
+    if (curSize < size) result += char;
+    curSize++;
+  }
+  return result;
 }
