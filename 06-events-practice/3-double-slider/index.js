64db9485629c75bc28565eb094bfb898ef7424d2
DmitriyBukreev <dmitribukree@yandex.ru>
2023-03-17 18:08:49 +0300

    Fixed destroy() method

--- 06-events-practice/3-double-slider/index.js
+++ 06-events-practice/3-double-slider/index.js
@@ -158,7 +158,7 @@ export default class DoubleSlider {
 
   destroy() {
     this.remove();
-    // this.element = null;
-    // this.subElements = {};
+    this.element = null;
+    this.subElements = {};
   }
 }
