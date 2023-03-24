f9b966f889ba204734df4d2dffda6b466e3c699d
DmitriyBukreev <dmitribukree@yandex.ru>
2023-03-24 22:41:23 +0300

    Implemented SortableList without drag&drop

--- 09-tests-for-frontend-apps/2-sortable-list/index.js
+++ 09-tests-for-frontend-apps/2-sortable-list/index.js
@@ -1,3 +1,46 @@
 export default class SortableList {
+  element;
 
+  onPointerDown = (event) => {
+    // TODO: Drag&Drop handler
+    // Consider list to be flat
+    let listItem = event.target.closest("li");
+    let target = event.target.closest("[data-delete-handle]");
+    if (target) {
+      listItem.remove();
+      return;
+    }
+    target = event.target.closest("[data-grab-handle]");
+    if (target) {
+      // Grab item
+      return;
+    }
+  };
+
+  constructor({ items = [] } = {}) {
+    this.render(items);
+    this.initListeners();
+    this.append = this.element.append;
+  }
+
+  render(items) {
+    const wrapper = document.createElement("div");
+    wrapper.innerHTML = `<ul class="sortable-list"></ul>`;
+    this.element = wrapper.firstElementChild;
+    this.element.append(...items);
+  }
+
+  initListeners() {
+    this.element.addEventListener("pointerdown", this.onPointerDown);
+  }
+
+  remove() {
+    if (!this.element) return;
+    this.element.remove();
+  }
+
+  destroy() {
+    this.remove();
+    this.element = null;
+  }
 }
